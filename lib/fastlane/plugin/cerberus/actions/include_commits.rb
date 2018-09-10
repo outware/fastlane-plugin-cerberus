8d8b1da067d4d7aaef19dbcc1ec91f0340b43479
Akshay Venkatesh <akshay.venkatesh@outware.com.au>
2018-09-10 10:41:03 +1000

    Run robocop and fix code

--- lib/fastlane/plugin/cerberus/actions/include_commits.rb
+++ lib/fastlane/plugin/cerberus/actions/include_commits.rb
@@ -1,97 +1,101 @@
 module Fastlane
- module Actions
+  module Actions
+    class IncludeCommitsAction < Action
+      def self.run(params)
+        regex = Regexp.new(params[:regex])
+        changelog = log(from: params[:from], to: params[:to], pretty: params[:pretty])
 
-  class IncludeCommitsAction < Action
+        if !changelog.to_s.empty?
+          tickets = tickets(log: changelog, regex: regex)
+          UI.important("Additional Issues: #{tickets.join("\n")}")
 
-   def self.run(params)
-    regex = Regexp.new params[:regex]
-    changelog = log(from: params[:from], to: params[:to], pretty: params[:pretty])
+          return tickets
+        else
+          UI.important('No issues found.')
+          return []
+        end
+      end
 
-    if !changelog.to_s.empty?
-     tickets = tickets(log: changelog, regex: regex)
-     UI.important "Additional Issues: #{tickets.join("\n")}"
+      #####################################################
+      # @!group Helpers
+      #####################################################
 
-     return tickets
-    else
-     UI.important 'No issues found.'
-     return []
+      def self.log(from:, to:, pretty:)
+        if to.to_s.empty? || from.to_s.empty?
+          UI.important('Git Tickets: log(to:, from:) cannot be nil')
+          return nil
+        end
+
+        other_action.changelog_from_git_commits(
+          between: [from, to],
+          pretty: pretty,
+          merge_commit_filtering: :exclude_merges.to_s
+        )
+      end
+
+      def self.tickets(log:, regex:)
+        return [] if log.to_s.empty?
+
+        log.each_line
+           .map(&:strip)
+           .grep(regex)
+           .flatten
+           .reject(&:empty?)
+           .uniq
+      end
+
+      #####################################################
+      # @!group Documentation
+      #####################################################
+
+      def self.details
+        'Extracts additional issues from the log'
+      end
+
+      def self.is_supported?(platform)
+        platform == :ios
+      end
+
+      def self.output
+        [String]
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(
+            key: :from,
+            env_name: 'FL_INCLUDE_COMMITS_FROM',
+            description:  'start commit',
+            optional: false,
+            default_value: ENV['FL_GIT_TICKETS_FROM'] || 'HEAD'
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :to,
+            env_name: 'FL_INCLUDE_COMMITS_TO',
+            description:  'end commit',
+            optional: false,
+            default_value: ENV['FL_GIT_TICKETS_TO'] || ENV['GIT_PREVIOUS_SUCCESSFUL_COMMIT'] || 'HEAD'
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :regex,
+            env_name: 'FL_INCLUDE_COMMITS_REGEX',
+            description:  'regex to only include to the change log',
+            optional: false,
+            default_value: ENV['FL_GIT_TICKETS_INCLUDE_REGEX'] || '([A-Z]+-\d+)'
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :pretty,
+            env_name: 'FL_INCLUDE_COMMITS_PRETTY_FORMAT',
+            description:  'git pretty format',
+            optional: false,
+            default_value: ENV['FL_GIT_TICKETS_PRETTY_FORMAT'] || '%s'
+          )
+        ]
+      end
+
+      def self.author
+        'Syd Srirak <sydney.srirak@outware.com.au>'
+      end
     end
-   end
-
-   def self.details
-    'Extracts additional issues from the log'
-   end
-
-   def self.is_supported?(platform)
-    platform == :ios
-   end
-
-   def self.output
-    [String]
-   end
-
-   def self.available_options
-    [
-     FastlaneCore::ConfigItem.new(
-      key: :from,
-      env_name: 'FL_INCLUDE_COMMITS_FROM',
-      description:  'start commit',
-      optional: false,
-      default_value: ENV['FL_GIT_TICKETS_FROM'] || 'HEAD',
-     ),
-     FastlaneCore::ConfigItem.new(
-      key: :to,
-      env_name: 'FL_INCLUDE_COMMITS_TO',
-      description:  'end commit',
-      optional: false,
-      default_value: ENV['FL_GIT_TICKETS_TO'] || ENV['GIT_PREVIOUS_SUCCESSFUL_COMMIT'] || 'HEAD'
-     ),
-     FastlaneCore::ConfigItem.new(
-      key: :regex,
-      env_name: 'FL_INCLUDE_COMMITS_REGEX',
-      description:  'regex to only include to the change log',
-      optional: false,
-      default_value: ENV['FL_GIT_TICKETS_INCLUDE_REGEX'] || '([A-Z]+-\d+)'
-     ),
-     FastlaneCore::ConfigItem.new(
-      key: :pretty,
-      env_name: 'FL_INCLUDE_COMMITS_PRETTY_FORMAT',
-      description:  'git pretty format',
-      optional: false,
-      default_value: ENV['FL_GIT_TICKETS_PRETTY_FORMAT'] || '%s'
-     )
-    ]
-   end
-
-   def self.author
-    'Syd Srirak <sydney.srirak@outware.com.au>'
-   end
-
-   private
-
-   def self.log(from:, to:, pretty:)
-    if to.to_s.empty? || from.to_s.empty?
-     UI.important 'Git Tickets: log(to:, from:) cannot be nil'
-     return nil
-    end
-
-    other_action.changelog_from_git_commits(
-     between: [from, to],
-     pretty: pretty,
-     merge_commit_filtering: :exclude_merges.to_s
-    )
-   end
-
-   def self.tickets(log:, regex:)
-     return [] if log.to_s.empty?
-
-     log.each_line
-     .map { |line| line.strip }
-     .grep(regex)
-     .flatten
-     .reject(&:empty?)
-     .uniq
-   end
   end
- end
 end
--- lib/fastlane/plugin/cerberus/actions/release_notes_action.rb
+++ lib/fastlane/plugin/cerberus/actions/release_notes_action.rb
@@ -2,127 +2,131 @@ require 'fastlane/action'
 require_relative '../helper/cerberus_helper'
 
 module Fastlane
- module Actions
+  module Actions
+    class ReleaseNotesAction < Action
+      def self.run(params)
+        @jira_helper = Helper::CerberusHelper.jira_helper(
+          host: params[:host],
+          username: params[:username],
+          password: params[:password],
+          context_path: params[:context_path],
+          disable_ssl_verification: params[:disable_ssl_verification]
+        )
+        issues = @jira_helper.get(issues: params[:issues])
+        Actions.lane_context[SharedValues::FL_CHANGELOG] = comment(issues: issues, include_commits: params[:include_commits], url: params[:buildURL])
+      end
 
-  class ReleaseNotesAction < Action
+      #####################################################
+      # @!group Helpers
+      #####################################################
 
-   def self.run(params)
-    @jira_helper = Helper::CerberusHelper.jira_helper(
-      host: params[:host], 
-      username: params[:username],
-      password: params[:password], 
-      context_path: params[:context_path], 
-      disable_ssl_verification: params[:disable_ssl_verification]
-    )
-    issues = @jira_helper.get(issues: params[:issues])
-    Actions.lane_context[SharedValues::FL_CHANGELOG] = comment(issues: issues, include_commits: params[:include_commits], url: params[:buildURL])
-   end
+      def self.format(issue:)
+        "[#{issue.key}](#{@jira_helper.url(issue: issue)}) - #{issue.summary}"
+      end
 
-   def self.details
-    'Creates a markdown friendly change log from the Jira issues and the Jenkins build url'
-   end
+      def self.format_issues(issues:, include_commits:)
+        issues.map { |issue| format(issue: issue) }
+              .concat(include_commits)
+              .map { |change| change.prepend("- ") }
+              .join("\n")
+      end
 
-   def self.is_supported?(platform)
-    platform == :ios
-   end
+      def self.comment(issues:, include_commits:, url:)
+        changes = format_issues(issues: issues, include_commits: include_commits)
+        changes = 'No changes included.' if changes.to_s.empty?
 
-   def self.output
-    String
-   end
+        changelog = [
+          '### Changelog',
+          '',
+          changes,
+          '',
+          "Built by [Jenkins](#{url})"
+        ].join("\n")
 
-   def self.available_options
-    [
-     FastlaneCore::ConfigItem.new(
-      key: :issues,
-      env_name: 'FL_RELEASE_NOTES_ISSUES',
-      description:  'Jira issues',
-      optional: false,
-      default_value: [],
-      type: Array
-     ),
-     FastlaneCore::ConfigItem.new(
-      key: :include_commits,
-      env_name: 'FL_HOCKEY_COMMENT_INCLUDE_COMMITS',
-      description:  'Additional commits',
-      optional: true,
-      default_value: [],
-      type: Array
-     ),
-     FastlaneCore::ConfigItem.new(
-      key: :buildURL,
-      env_name: 'FL_RELEASE_NOTES_BUILD_URL',
-      description:  'Link to the ci build',
-      optional: false,
-      default_value: ENV['BUILD_URL'],
-     ),
-     FastlaneCore::ConfigItem.new(
-      key: :username,
-      env_name: 'FL_JIRA_USERNAME',
-      description:  'Jira user',
-      optional: false
-     ),
-     FastlaneCore::ConfigItem.new(
-      key: :password,
-      env_name: 'FL_JIRA_PASSWORD',
-      description:  'Jira user',
-      optional: false
-     ),
-     FastlaneCore::ConfigItem.new(
-      key: :host,
-      env_name: 'FL_JIRA_HOST',
-      description:  'Jira location',
-      optional: false
-     ),
-     FastlaneCore::ConfigItem.new(
-      key: :context_path,
-      env_name: 'FL_JIRA_CONTEXT_PATH',
-      description:  'Jira context path',
-      optional: false,
-      default_value: ''
-     ),
-     FastlaneCore::ConfigItem.new(
-      key: :disable_ssl_verification,
-      env_name: 'FL_JIRA_DISABLE_SSL_VERIFICATION',
-      description:  'Jira SSL Verification mode',
-      optional: false,
-      default_value: false,
-      type: Boolean
-     )
-    ]
-   end
+        UI.important(changelog)
+        return changelog
+      end
 
-   def self.author
-    'Harry Singh <harry.singh@outware.com.au>'
-   end
+      #####################################################
+      # @!group Documentation
+      #####################################################
 
-   private
+      def self.details
+        'Creates a markdown friendly change log from the Jira issues and the Jenkins build url'
+      end
 
-   def self.format(issue:)
-    "[#{issue.key}](#{@jira_helper.url(issue: issue)}) - #{issue.summary}"
-   end
+      def self.is_supported?(platform)
+        platform == :ios
+      end
 
-   def self.format_issues(issues:, include_commits:)
-     issues.map { |issue| format(issue: issue) }
-     .concat(include_commits)
-     .map { |change| change.prepend("- ") }
-     .join("\n")
-   end
+      def self.output
+        String
+      end
 
-   def self.comment(issues:, include_commits:, url:)
-    changes = format_issues(issues: issues, include_commits: include_commits)
-    changes = 'No changes included.' if changes.to_s.empty?
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(
+            key: :issues,
+            env_name: 'FL_RELEASE_NOTES_ISSUES',
+            description:  'Jira issues',
+            optional: false,
+            default_value: [],
+            type: Array
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :include_commits,
+            env_name: 'FL_HOCKEY_COMMENT_INCLUDE_COMMITS',
+            description:  'Additional commits',
+            optional: true,
+            default_value: [],
+            type: Array
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :buildURL,
+            env_name: 'FL_RELEASE_NOTES_BUILD_URL',
+            description:  'Link to the ci build',
+            optional: false,
+            default_value: ENV['BUILD_URL']
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :username,
+            env_name: 'FL_JIRA_USERNAME',
+            description:  'Jira user',
+            optional: false
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :password,
+            env_name: 'FL_JIRA_PASSWORD',
+            description:  'Jira user',
+            optional: false
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :host,
+            env_name: 'FL_JIRA_HOST',
+            description:  'Jira location',
+            optional: false
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :context_path,
+            env_name: 'FL_JIRA_CONTEXT_PATH',
+            description:  'Jira context path',
+            optional: false,
+            default_value: ''
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :disable_ssl_verification,
+            env_name: 'FL_JIRA_DISABLE_SSL_VERIFICATION',
+            description:  'Jira SSL Verification mode',
+            optional: false,
+            default_value: false,
+            type: Boolean
+          )
+        ]
+      end
 
-    changelog = [
-      '### Changelog',
-      '',
-      changes,
-      '',
-      "Built by [Jenkins](#{url})"
-    ].join("\n")
-
-    UI.important changelog
-    return changelog
-   end
+      def self.author
+        'Harry Singh <harry.singh@outware.com.au>'
+      end
+    end
   end
- end
 end
